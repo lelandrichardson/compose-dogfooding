6314ae07c400c204b5f943bb8320c5b507278ab4
Leland Richardson <lelandr@google.com>
2020-09-21 16:14:08 -0700

    Work from Ep 3

--- app/src/main/java/com/example/dogfood/MovieCarousel.kt
+++ app/src/main/java/com/example/dogfood/MovieCarousel.kt
@@ -1,8 +1,13 @@
 package com.example.dogfood
 
+import androidx.compose.animation.asDisposableClock
+import androidx.compose.animation.core.TargetAnimation
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.animation.FlingConfig
+import androidx.compose.foundation.animation.defaultFlingConfig
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.ScrollableController
 import androidx.compose.foundation.gestures.rememberScrollableController
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
@@ -14,29 +19,28 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.ConfigurationAmbient
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.*
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawOpacity
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.*
-import androidx.compose.ui.platform.DensityAmbient
-import androidx.compose.ui.platform.InspectableParameter
-import androidx.compose.ui.platform.ParameterElement
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.unit.*
 import androidx.ui.tooling.preview.Preview
 import dev.chrisbanes.accompanist.coil.CoilImage
 import java.lang.Math.abs
+import kotlin.math.max
 import kotlin.math.roundToInt
 
 data class Movie(
         val title: String,
         val posterUrl: String,
         val bgUrl: String,
+        val color: Color,
         val chips: List<String>
 )
 
@@ -45,18 +49,21 @@ val movies = listOf(
         title = "Good Boys",
         posterUrl = "https://m.media-amazon.com/images/M/MV5BMTc1NjIzODAxMF5BMl5BanBnXkFtZTgwMTgzNzk1NzM@._V1_.jpg",
         bgUrl = "https://m.media-amazon.com/images/M/MV5BMTc1NjIzODAxMF5BMl5BanBnXkFtZTgwMTgzNzk1NzM@._V1_.jpg",
+        color = Color.Red,
         chips = listOf("Action", "Drama", "History")
     ),
     Movie(
         title = "Joker",
         posterUrl = "https://i.etsystatic.com/15963200/r/il/25182b/2045311689/il_794xN.2045311689_7m2o.jpg",
         bgUrl = "https://images-na.ssl-images-amazon.com/images/I/61gtGlalRvL._AC_SY741_.jpg",
+        color = Color.Blue,
         chips = listOf("Action", "Drama", "History")
     ),
     Movie(
         title = "The Hustle",
         posterUrl = "https://m.media-amazon.com/images/M/MV5BMTc3MDcyNzE5N15BMl5BanBnXkFtZTgwNzE2MDE0NzM@._V1_.jpg",
         bgUrl = "https://m.media-amazon.com/images/M/MV5BMTc3MDcyNzE5N15BMl5BanBnXkFtZTgwNzE2MDE0NzM@._V1_.jpg",
+        color = Color.Yellow,
         chips = listOf("Action", "Drama", "History")
     )
 )
@@ -73,11 +80,33 @@ fun Screen() {
     val screenHeight = configuration.screenHeightDp.dp
     val screenHeightPx = with(density) { screenHeight.toPx() }
     var offset by remember { mutableStateOf(0f) }
-    val ctrlr = rememberScrollableController {
-        offset += it
-        it
+    val posterWidthDp = screenWidth * 0.6f
+    val posterSpacingPx = with(density) { posterWidthDp.toPx() + 20.dp.toPx() }
+    val indexFraction = -1 * offset / posterSpacingPx
+    val flingConfig = defaultFlingConfig {
+        TargetAnimation((it / posterSpacingPx).roundToInt() * posterSpacingPx)
     }
-    val indexFraction = -1 * offset / screenWidthPx
+    val upperBound = 0f
+    val lowerBound = -1 * (movies.size - 1) * posterSpacingPx
+    val ctrlr = rememberScrollableController(flingConfig) {
+        val target = offset + it
+        when {
+            target > upperBound -> {
+                val consumed = upperBound - offset
+                offset = upperBound
+                consumed
+            }
+            target < lowerBound -> {
+                val consumed = lowerBound - offset
+                offset = lowerBound
+                consumed
+            }
+            else -> {
+                offset = target
+                it
+            }
+        }
+    }
     Stack(
         Modifier
             .background(Color.Black)
@@ -88,11 +117,29 @@ fun Screen() {
             )
     )  {
         movies.forEachIndexed { index, movie ->
-            val opacity = if (indexFraction.roundToInt() == index) 1f else 0f
+            val isInRange = (index >= indexFraction - 1 && indexFraction + 1 > index)
+            val opacity = if (isInRange) 1f else 0f
+            val shape = when {
+                !isInRange -> RectangleShape
+                // 0, 0.25 -> 0.25, 1f
+                // 1, 0.25 -> 0f, 0.25
+                index <= indexFraction -> {
+                    val fraction = indexFraction - index
+                    FractionalRectangleShape(fraction.coerceIn(0f, 1f-Float.MIN_VALUE), 1f)
+                }
+                else -> {
+                    val fraction = indexFraction - index + 1
+                    FractionalRectangleShape(0f, fraction.coerceIn(Float.MIN_VALUE, 1f))
+                }
+            }
             CoilImage(
                     data = movie.bgUrl,
                     modifier = Modifier
-                            .drawOpacity(opacity)
+                            .drawLayer(
+                                    alpha = opacity,
+                                    shape = shape,
+                                    clip = true
+                            )
                             .fillMaxWidth()
                             .aspectRatio(posterAspectRatio)
             )
@@ -105,17 +152,44 @@ fun Screen() {
                     .fillMaxHeight(0.6f)
         )
         movies.forEachIndexed { index, movie ->
-            val center = screenWidthPx * index
-            val distFromCenter = abs(offset - center) / screenWidthPx
+            val center = posterSpacingPx * index
+            val distFromCenter = abs(offset + center) / posterSpacingPx
             MoviePoster(
                     movie = movie,
                     modifier = Modifier
-                            .offset(getX = { center + offset }, getY = { lerp(0f, -50f, distFromCenter) })
-                            .width(screenWidth * .75f)
+                            .offset(getX = { center + offset }, getY = { lerp(0f, 50f, distFromCenter) })
+                            .width(posterWidthDp)
                             .gravity(Alignment.BottomCenter)
             )
         }
     }
+}
+
+@Composable
+fun rememberScrollableController(
+        flingConfig: FlingConfig = defaultFlingConfig(),
+        consumeScrollDelta: (Float) -> Float
+): ScrollableController {
+    val clocks = AnimationClockAmbient.current.asDisposableClock()
+    return remember(clocks, flingConfig) {
+        ScrollableController(consumeScrollDelta, flingConfig, clocks)
+    }
+}
+
+fun FractionalRectangleShape(startFraction: Float, endFraction: Float) = object : Shape {
+    override fun createOutline(size: Size, density: Density) =
+            Outline.Rectangle(
+                    Rect(
+                            top = 0f,
+                            left = startFraction * size.width,
+                            bottom = size.height,
+                            right = endFraction * size.width
+                    )
+            )
+}
+
+fun lerp(start: Float, stop: Float, fraction: Float): Float {
+    return (1 - fraction) * start + fraction * stop
 }
 
 fun Modifier.verticalGradient(vararg colors: ColorStop) = this then object : DrawModifier, InspectableParameter {
