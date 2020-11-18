14cfa2c3255603c5300a86f25cdd6b3234c590c5
Leland Richardson <lelandr@google.com>
2020-11-18 13:47:50 -0800

    Ep 12

--- project-management-app/app/src/main/java/com/example/pmapp/CreateTaskScreen.kt
+++ project-management-app/app/src/main/java/com/example/pmapp/CreateTaskScreen.kt
@@ -1,24 +1,32 @@
 package com.example.pmapp
 
-import androidx.compose.foundation.Icon
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.IconButton
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Attachment
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Stop
 import androidx.compose.runtime.*
+import androidx.compose.runtime.State
+import androidx.compose.runtime.snapshots.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pmapp.ui.bgColor
+import com.example.pmapp.ui.primaryGreen
+import dev.chrisbanes.accompanist.coil.CoilImage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlin.math.sqrt
 
-val bgColor = Color(0xFF33354E)
 
 class Client(
     val id: Int,
@@ -45,6 +53,20 @@ val mockClients = listOf(
 )
 
 
+class Attachment(
+  val name: String,
+  val preview: String,
+  val size: Int
+) {
+  val progress: Float get() = 0.8f
+}
+
+val mockAttachment = Attachment(
+  name = "Reference_1",
+  preview = "https://i.pravatar.cc/48?img=5",
+  size = 168,
+)
+
 @Composable
 fun CreateTaskScreen(clients: List<Client> = mockClients) {
   var projectName by remember { mutableStateOf("Create additional pages") }
@@ -52,44 +74,120 @@ fun CreateTaskScreen(clients: List<Client> = mockClients) {
   var startDate by remember { mutableStateOf("") }
   var endDate by remember { mutableStateOf("") }
   var category by remember { mutableStateOf("Design") }
+  val attachments = remember { mutableStateListOf(mockAttachment) }
 
-  ScrollableColumn(
-      Modifier
-          .background(bgColor)
-          .padding(horizontal = 16.dp, vertical = 20.dp)
-  ) {
-    FieldLabel("PROJECT NAME")
-    ClientDropdownField(
-        selectedValue = client,
-        values = clients,
-        onValueChange = { client = it },
-    )
-    FieldLabel("PROJECT NAME")
-    PMTextField(value = projectName, onValueChange = { projectName = it })
-    Row {
-      FieldLabel("START/END DATES")
-      DatePicker(value = startDate, onValueChange = { startDate = it })
-      DatePicker(value = endDate, onValueChange = { endDate = it })
+  ScrollableColumn {
+    Surface(
+      color = bgColor,
+      contentColor = Color.White,
+    ) {
+      Column {
+        Column(Modifier.padding(horizontal = 16.dp, vertical = 20.dp)) {
+          Field("PROJECT NAME") {
+            ClientDropdownField(
+                selectedValue = client,
+                values = clients,
+                onValueChange = { client = it },
+            )
+          }
+
+          Field("PROJECT NAME") {
+            PMTextField(
+              modifier = Modifier.weight(1f),
+              value = projectName,
+              onValueChange = { projectName = it }
+            )
+          }
+
+          Field("START/END DATES") {
+            DatePicker(value = startDate, onValueChange = { startDate = it })
+            Text(" - ")
+            DatePicker(value = endDate, onValueChange = { endDate = it })
+          }
+
+          Field("ASSIGNEE") {
+            AvatarList(
+                size = 40.dp,
+                users = mockProject.users,
+                onUserClick = {},
+                showAddButton = true,
+                onAddClick = {}
+            )
+          }
+
+          Field("CATEGORY") {
+            RadioChips(
+                selectedValue = category,
+                onSelectedChange = { category = it },
+                values = listOf("Design", "Frontend", "Backend")
+            )
+          }
+        }
+
+        Surface(
+          shape = RoundedCornerShape(topLeft = 24.dp, topRight = 24.dp),
+          color = Color.White
+        ) {
+          Column(Modifier.padding(horizontal = 16.dp, vertical = 24.dp)) {
+            Text("Description")
+            IconButton(onClick = {}) {
+              Icon(Icons.Default.Attachment, tint = primaryGreen)
+            }
+            Text("ATTACHMENTS")
+            for (attachment in attachments) {
+              AttachmentProgress(attachment)
+            }
+            Button(
+              modifier = Modifier.fillMaxWidth(),
+              onClick = {}
+            ) {
+              Text("CREATE TASK")
+            }
+          }
+        }
+      }
     }
-    FieldLabel("ASSIGNEE")
-    AvatarList(
-        size = 40.dp,
-        users = mockProject.users,
-        onUserClick = {},
-        showAddButton = true,
-        onAddClick = {}
-    )
-    FieldLabel("CATEGORY")
-    RadioChips(
-        selectedValue = category,
-        onSelectedChange = { category = it },
-        values = listOf("Design", "Frontend", "Backend")
-    )
   }
 }
 
-@Composable fun FieldLabel(label: String) {
-  Text(label)
+@Composable fun AttachmentProgress(attachment: Attachment) {
+  Row {
+    CoilImage(
+      modifier = Modifier
+        .size(48.dp)
+        .clip(CircleShape)
+        .background(Color.White),
+      data = attachment.preview,
+    )
+    Spacer(Modifier.size(8.dp))
+    Column(Modifier.weight(1f)) {
+      Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(attachment.name)
+        Text("${attachment.size} KB")
+      }
+      LinearProgressIndicator(
+        modifier = Modifier.fillMaxWidth(),
+        progress = attachment.progress,
+        color = primaryGreen
+      )
+    }
+    IconButton(onClick = {}) {
+      Icon(Icons.Default.Stop)
+    }
+  }
+}
+
+@Composable
+inline fun Field(label: String, content: @Composable RowScope.() -> Unit) {
+  Column(Modifier.padding(vertical = 8.dp)) {
+    Text(
+      label,
+      color = Color.White.copy(alpha = 0.7f),
+      fontSize = 10.sp,
+      modifier = Modifier.padding(bottom=8.dp)
+    )
+    Row { content() }
+  }
 }
 
 @Composable fun RadioChips(
@@ -97,17 +195,29 @@ fun CreateTaskScreen(clients: List<Client> = mockClients) {
     values: List<String>,
     onSelectedChange: (String) -> Unit
 ) {
-  Row {
+  val selectedColor = ButtonConstants.defaultButtonColors(backgroundColor = primaryGreen, contentColor = Color.White)
+  val unselectedColor = ButtonConstants.defaultButtonColors(backgroundColor = Color.White)
+  Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
     for(value in values) {
       val selected = value == selectedValue
-      IconButton(
-          modifier = Modifier.background(if (selected) Color.Green else Color.White),
-          onClick = { onSelectedChange(value) },
+      Button(
+        contentPadding = PaddingValues(start = 8.dp, end = 24.dp, top = 4.dp, bottom = 4.dp),
+        colors = if (selected) selectedColor else unselectedColor,
+        shape = RoundedCornerShape(8.dp),
+        onClick = { onSelectedChange(value) },
       ) {
-          Row {
-            if (selected) Icon(Icons.Default.Check)
-            Text(value, color = if (selected) Color.White else Color.Black)
-          }
+        if (selected)
+          Icon(
+            asset = Icons.Default.Check,
+            modifier = Modifier.size(16.dp)
+          )
+        else
+          Spacer(Modifier.size(16.dp))
+        Text(
+          value,
+          modifier = Modifier.padding(start=4.dp),
+          color = if (selected) Color.White else Color.Black
+        )
       }
     }
   }
@@ -120,14 +230,24 @@ fun ClientDropdownField(
     onValueChange: (Client) -> Unit,
     values: List<Client>,
 ) {
-
+  Row {
+    CoilImage(
+      modifier = Modifier
+        .size(48.dp)
+        .clip(CircleShape)
+        .background(Color.White),
+      data = "https://i.pravatar.cc/200?img=2"
+    )
+    Spacer(Modifier.size(6.dp))
+    PMTextField(value = selectedValue.name, onValueChange = {})
+  }
 }
 
 @Composable fun DatePicker(
   value: String,
   onValueChange: (String) -> Unit
 ) {
-  TextField(value = value, onValueChange = onValueChange)
+  PMTextField(value = value, onValueChange = onValueChange)
 }
 
 @Composable
@@ -142,8 +262,17 @@ fun <T> DropdownField(
 
 @Composable
 fun PMTextField(
+    modifier: Modifier = Modifier,
     value: String,
     onValueChange: (String) -> Unit,
 ) {
-  TextField(value = value, onValueChange = onValueChange)
+  TextField(
+    modifier = modifier,
+    value = value,
+    onValueChange = onValueChange,
+    textStyle = TextStyle(color = Color.White),
+    activeColor = Color.White,
+    inactiveColor = Color.White.copy(alpha=0.7f),
+    backgroundColor = bgColor
+  )
 }
--- project-management-app/app/src/main/java/com/example/pmapp/ui/Color.kt
+++ project-management-app/app/src/main/java/com/example/pmapp/ui/Color.kt
@@ -6,3 +6,7 @@ val purple200 = Color(0xFFBB86FC)
 val purple500 = Color(0xFF6200EE)
 val purple700 = Color(0xFF3700B3)
 val teal200 = Color(0xFF03DAC5)
+
+
+val bgColor = Color(0xFF33354E)
+val primaryGreen = Color(0xFF2CC09C)
