541afbe8762fbe4e6a5bd4efe5d296c8b4e59c39
Leland Richardson <lelandr@google.com>
2021-03-04 13:13:25 -0800

    transparent status bar

--- Countdown/app/src/main/java/com/example/countdown/MainActivity.kt
+++ Countdown/app/src/main/java/com/example/countdown/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.countdown
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,30 +10,52 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.countdown.ui.theme.CountdownTheme
+import android.view.WindowManager
+import android.view.View
+import android.view.Window
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    window.makeTransparentStatusBar()
     setContent {
       CountdownTheme {
         // A surface container using the 'background' color from the theme
         Surface(color = MaterialTheme.colors.background) {
-          Greeting("Android")
+          Text("Hello World", Modifier.padding(50.dp))
         }
       }
     }
   }
 }
 
-@Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Window.makeTransparentStatusBar() {
+  markAttributes(
+    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+    true
+  )
+  decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+      or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+      or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+  markAttributes(
+    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+        or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+    false
+  )
+  statusBarColor = Color.TRANSPARENT
+  navigationBarColor = Color.TRANSPARENT
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-  CountdownTheme {
-    Greeting("Android")
+fun Window.markAttributes(bits: Int, value: Boolean) {
+  val params = attributes
+  if (value) {
+    params.flags = params.flags or bits
+  } else {
+    params.flags = params.flags and bits.inv()
   }
+  attributes = params
 }
